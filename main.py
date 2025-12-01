8a35320af3673d4ee146f762cddc10d2ab81b10b
karolinagudyka <u56121@student.wat.edu.pl>
2025-12-01 17:24:08 +0100

    feat: update fonts and color palette

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from tkinter import *
+from tkinter import font
 import tkintermapview
 
 root = Tk()
 root.title("Projekt systemu do zarządzania jednostkami policji i policjantami przypisanymi do danej jednostki")
-root.geometry("2304x1536")
+root.geometry("1300x900")
+
+default_font = font.Font(family="Times New Roman", size=10)
+label_font = font.Font(family="Times New Roman", size=12, weight="bold")
 
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
@@ -16,13 +20,13 @@ root.rowconfigure(0, weight=0)
 root.rowconfigure(1, weight=0)
 root.rowconfigure(2, weight=1)
 
-ramka_jednostki = Frame(root, padx=5, pady=5)
-ramka_formularz_jednostki = Frame(root, padx=5, pady=5)
-ramka_pracownicy = Frame(root, padx=5, pady=5)
-ramka_formularz_pracownicy = Frame(root, padx=5, pady=5)
-ramka_incydenty = Frame(root, padx=5, pady=5)
-ramka_formularz_incydenty = Frame(root, padx=5, pady=5)
-ramka_szczegoly_obiektu = Frame(root, padx=5, pady=5)
+ramka_jednostki = Frame(root, padx=5, pady=5, bg="#f2d0d7")
+ramka_formularz_jednostki = Frame(root, padx=5, pady=5, bg="#f2d0d7")
+ramka_pracownicy = Frame(root, padx=5, pady=5, bg="#eddff7")
+ramka_formularz_pracownicy = Frame(root, padx=5, pady=5, bg="#eddff7")
+ramka_incydenty = Frame(root, padx=5, pady=5, bg="#f0c2e3")
+ramka_formularz_incydenty = Frame(root, padx=5, pady=5, bg="#f0c2e3")
+ramka_szczegoly_obiektu = Frame(root, padx=5, pady=5, bg="#f7e9f3")
 ramka_mapa = Frame(root)
 
 ramka_jednostki.grid(row=0, column=0, sticky="nsew")
@@ -36,19 +40,19 @@ ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=6, sticky="ew", pady=10
 ramka_mapa.grid(row=2, column=0, columnspan=6, sticky="nsew")
 
 # RAMKA LISTA JEDNOSTEK
-label_lista_jednostek = Label(ramka_jednostki, text="Lista jednostek policji")
+label_lista_jednostek = Label(ramka_jednostki, text="Lista jednostek policji", font=label_font, bg="#f2d0d7")
 label_lista_jednostek.grid(row=0, column=0, columnspan=3, sticky="ew")
 
-list_box_lista_jednostek = Listbox(ramka_jednostki)
+list_box_lista_jednostek = Listbox(ramka_jednostki, font=default_font)
 list_box_lista_jednostek.grid(row=1, column=0, columnspan=3, sticky="nsew")
 
-buttom_dodaj_jednostke = Button(ramka_jednostki, text="Dodaj jednostkę")
-buttom_dodaj_jednostke.grid(row=2, column=0, sticky="ew")
+buttom_szczegoly_jednostki = Button(ramka_jednostki, text="Wyświetl szczegóły", font=default_font)
+buttom_szczegoly_jednostki.grid(row=2, column=0, sticky="ew")
 
-buttom_usun_jednostke = Button(ramka_jednostki, text="Usuń jednostkę")
+buttom_usun_jednostke = Button(ramka_jednostki, text="Usuń", font=default_font)
 buttom_usun_jednostke.grid(row=2, column=1, sticky="ew")
 
-buttom_aktualizuj_jednostke = Button(ramka_jednostki, text="Aktualizuj jednostkę")
+buttom_aktualizuj_jednostke = Button(ramka_jednostki, text="Aktualizuj", font=default_font)
 buttom_aktualizuj_jednostke.grid(row=2, column=2, sticky="ew")
 
 ramka_jednostki.columnconfigure(0, weight=1)
@@ -57,47 +61,47 @@ ramka_jednostki.columnconfigure(2, weight=1)
 ramka_jednostki.rowconfigure(1, weight=1)
 
 #RAMKA FORMULARZ JEDNOSTEK
-label_formularz_jednostek = Label(ramka_formularz_jednostki, text="Formularz - jednostki: ")
+label_formularz_jednostek = Label(ramka_formularz_jednostki, text="Formularz - jednostki: ", font=label_font, bg="#f2d0d7")
 label_formularz_jednostek.grid(row=0, column=0, columnspan=2, sticky="ew")
 
-label_nazwa_jednostki = Label(ramka_formularz_jednostki, text= "Nazwa: ")
+label_nazwa_jednostki = Label(ramka_formularz_jednostki, text= "Nazwa: ", font=default_font, bg="#f2d0d7")
 label_nazwa_jednostki.grid(row=1, column=0, sticky=W)
 
-label_ulica_jednostki = Label(ramka_formularz_jednostki, text="Ulica: ")
+label_ulica_jednostki = Label(ramka_formularz_jednostki, text="Ulica: ", font=default_font, bg="#f2d0d7")
 label_ulica_jednostki.grid(row=2, column=0, sticky=W)
 
-label_miasto_jednostki = Label(ramka_formularz_jednostki, text="Miasto: ")
+label_miasto_jednostki = Label(ramka_formularz_jednostki, text="Miasto: ", font=default_font, bg="#f2d0d7")
 label_miasto_jednostki.grid(row=3, column=0, sticky=W)
 
-entry_nazwa_jednostki = Entry(ramka_formularz_jednostki)
+entry_nazwa_jednostki = Entry(ramka_formularz_jednostki, font=default_font)
 entry_nazwa_jednostki.grid(row=1, column=1, sticky="ew")
 
-entry_ulica_jednostki = Entry(ramka_formularz_jednostki)
+entry_ulica_jednostki = Entry(ramka_formularz_jednostki, font=default_font)
 entry_ulica_jednostki.grid(row=2, column=1, sticky="ew")
 
-entry_miasto_jednostki = Entry(ramka_formularz_jednostki)
+entry_miasto_jednostki = Entry(ramka_formularz_jednostki, font=default_font)
 entry_miasto_jednostki.grid(row=3, column=1, sticky="ew")
 
-button_dodaj_jednostke = Button(ramka_formularz_jednostki, text="Dodaj obiekt")
+button_dodaj_jednostke = Button(ramka_formularz_jednostki, text="Dodaj obiekt", font=default_font)
 button_dodaj_jednostke.grid(row=4, column=0, columnspan=2, sticky="ew")
 
 ramka_formularz_jednostki.columnconfigure(1, weight=1)
 
 
 #RAMKA LISTA PRACOWNIKÓW
-label_lista_pracownikow = Label(ramka_pracownicy, text="Lista pracowników policji")
+label_lista_pracownikow = Label(ramka_pracownicy, text="Lista pracowników policji", font=label_font, bg="#eddff7")
 label_lista_pracownikow.grid(row=0, column=0, columnspan=3, sticky="ew")
 
-list_box_lista_pracownikow = Listbox(ramka_pracownicy)
+list_box_lista_pracownikow = Listbox(ramka_pracownicy, font=default_font)
 list_box_lista_pracownikow.grid(row=1, column=0, columnspan=3, sticky="nsew")
 
-buttom_dodaj_pracownika= Button(ramka_pracownicy, text="Dodaj pracownika")
-buttom_dodaj_pracownika.grid(row=2, column=0, sticky="ew")
+buttom_szczegoly_pracownika= Button(ramka_pracownicy, text="Wyświetl szczegóły", font=default_font)
+buttom_szczegoly_pracownika.grid(row=2, column=0, sticky="ew")
 
-buttom_usun_pracownika = Button(ramka_pracownicy, text="Usuń pracownika")
+buttom_usun_pracownika = Button(ramka_pracownicy, text="Usuń", font=default_font)
 buttom_usun_pracownika.grid(row=2, column=1, sticky="ew")
 
-buttom_aktualizuj_pracownika = Button(ramka_pracownicy, text="Aktualizuj pracownika")
+buttom_aktualizuj_pracownika = Button(ramka_pracownicy, text="Aktualizuj", font=default_font)
 buttom_aktualizuj_pracownika.grid(row=2, column=2, sticky="ew")
 
 ramka_pracownicy.columnconfigure(0, weight=1)
@@ -106,47 +110,47 @@ ramka_pracownicy.columnconfigure(2, weight=1)
 ramka_pracownicy.rowconfigure(1, weight=1)
 
 #RAMKA FORMULARZ PRACOWNIKÓW
-label_formularz_pracownicy = Label(ramka_formularz_pracownicy, text="Formularz - pracownicy: ")
+label_formularz_pracownicy = Label(ramka_formularz_pracownicy, text="Formularz - pracownicy: ", font=label_font, bg="#eddff7")
 label_formularz_pracownicy.grid(row=0, column=0, columnspan=2, sticky="ew")
 
-label_imie_pracownika = Label(ramka_formularz_pracownicy, text= "Imię: ")
+label_imie_pracownika = Label(ramka_formularz_pracownicy, text= "Imię: ", font=default_font, bg="#eddff7")
 label_imie_pracownika.grid(row=1, column=0, sticky=W)
 
-label_nazwisko_pracownika = Label(ramka_formularz_pracownicy, text="Nazwisko: ")
+label_nazwisko_pracownika = Label(ramka_formularz_pracownicy, text="Nazwisko: ", font=default_font, bg="#eddff7")
 label_nazwisko_pracownika.grid(row=2, column=0, sticky=W)
 
-label_miasto_pracownika = Label(ramka_formularz_pracownicy, text="Miasto: ")
+label_miasto_pracownika = Label(ramka_formularz_pracownicy, text="Miasto: ", font=default_font, bg="#eddff7")
 label_miasto_pracownika.grid(row=3, column=0, sticky=W)
 
-entry_imie_pracownika = Entry(ramka_formularz_pracownicy)
+entry_imie_pracownika = Entry(ramka_formularz_pracownicy, font=default_font)
 entry_imie_pracownika.grid(row=1, column=1, sticky="ew")
 
-entry_nazwisko_pracownika = Entry(ramka_formularz_pracownicy)
+entry_nazwisko_pracownika = Entry(ramka_formularz_pracownicy, font=default_font)
 entry_nazwisko_pracownika.grid(row=2, column=1, sticky="ew")
 
-entry_miasto_pracownika = Entry(ramka_formularz_pracownicy)
+entry_miasto_pracownika = Entry(ramka_formularz_pracownicy, font=default_font)
 entry_miasto_pracownika.grid(row=3, column=1, sticky="ew")
 
-button_dodaj_pracownika = Button(ramka_formularz_pracownicy, text="Dodaj obiekt")
+button_dodaj_pracownika = Button(ramka_formularz_pracownicy, text="Dodaj obiekt", font=default_font)
 button_dodaj_pracownika.grid(row=4, column=0, columnspan=2, sticky="ew")
 
 ramka_formularz_pracownicy.columnconfigure(1, weight=1)
 
 
 #RAMKA LISTA INCYDENTÓW
-label_lista_incydentow = Label(ramka_incydenty, text="Lista incydentów")
+label_lista_incydentow = Label(ramka_incydenty, text="Lista incydentów", font=label_font, bg="#f0c2e3")
 label_lista_incydentow.grid(row=0, column=0, columnspan=3, sticky="ew")
 
-list_box_lista_incydentow = Listbox(ramka_incydenty)
+list_box_lista_incydentow = Listbox(ramka_incydenty, font=default_font)
 list_box_lista_incydentow.grid(row=1, column=0, columnspan=3, sticky="nsew")
 
-buttom_dodaj_incydent= Button(ramka_incydenty, text="Dodaj incydent")
-buttom_dodaj_incydent.grid(row=2, column=0, sticky="ew")
+buttom_szczegoly_incydentu= Button(ramka_incydenty, text="Wyświetl szczegóły", font=default_font)
+buttom_szczegoly_incydentu.grid(row=2, column=0, sticky="ew")
 
-buttom_usun_incydent = Button(ramka_incydenty, text="Usuń incydent")
+buttom_usun_incydent = Button(ramka_incydenty, text="Usuń", font=default_font)
 buttom_usun_incydent.grid(row=2, column=1, sticky="ew")
 
-buttom_aktualizuj_incydent = Button(ramka_incydenty, text="Aktualizuj incydent")
+buttom_aktualizuj_incydent = Button(ramka_incydenty, text="Aktualizuj", font=default_font)
 buttom_aktualizuj_incydent.grid(row=2, column=2, sticky="ew")
 
 ramka_incydenty.columnconfigure(0, weight=1)
@@ -155,10 +159,10 @@ ramka_incydenty.columnconfigure(2, weight=1)
 ramka_incydenty.rowconfigure(1, weight=1)
 
 #RAMKA FORMULARZ INCYDENTÓW
-label_formularz_incydentów = Label(ramka_formularz_incydenty, text="Formularz - pracownicy: ")
+label_formularz_incydentów = Label(ramka_formularz_incydenty, text="Formularz - incydenty: ", font=label_font, bg="#f0c2e3")
 label_formularz_incydentów.grid(row=0, column=0, columnspan=2, sticky="ew")
 
-label_nazwa_incydentu = Label(ramka_formularz_incydenty, text= "Nazwa: ")
+label_nazwa_incydentu = Label(ramka_formularz_incydenty, text= "Nazwa: ", font=default_font, bg="#f0c2e3")
 label_nazwa_incydentu.grid(row=1, column=0, sticky=W)
 
 # label_nazwisko_pracownika = Label(ramka_formularz_jednostki, text="Nazwisko: ")
@@ -167,7 +171,7 @@ label_nazwa_incydentu.grid(row=1, column=0, sticky=W)
 # label_miasto_pracownika = Label(ramka_formularz_jednostki, text="Miasto: ")
 # label_miasto_pracownika.grid(row=3, column=0, sticky=W)
 
-entry_nazwa_incydentu = Entry(ramka_formularz_incydenty)
+entry_nazwa_incydentu = Entry(ramka_formularz_incydenty, font=default_font)
 entry_nazwa_incydentu.grid(row=1, column=1, sticky="ew")
 
 # entry_nazwisko_pracownika = Entry(ramka_formularz_jednostki)
@@ -176,31 +180,31 @@ entry_nazwa_incydentu.grid(row=1, column=1, sticky="ew")
 # entry_miasto_pracownika = Entry(ramka_formularz_jednostki)
 # entry_miasto_pracownika.grid(row=3, column=1)
 
-button_dodaj_pracownika = Button(ramka_formularz_incydenty, text="Dodaj obiekt")
+button_dodaj_pracownika = Button(ramka_formularz_incydenty, text="Dodaj obiekt", font=default_font)
 button_dodaj_pracownika.grid(row=2, column=0, columnspan=2, sticky="ew")
-
+ramka_formularz_incydenty.columnconfigure(1, weight=1)
 
 # RAMKA SZCZEGÓŁY OBIEKTU
-label_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Szczegóły obiektu: ")
-label_szczegoly_obiektu.grid(row=0, column=0, columnspan=6, sticky=W)
+label_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Szczegóły obiektu: ", font=label_font, bg="#f7e9f3")
+label_szczegoly_obiektu.grid(row=0, column=0, columnspan=6, sticky="ew")
 
-label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text= "Imie: ")
-label_imie_szczegoly_obiektu.grid(row=1, column=0, sticky=W)
+label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text= "Imie: ", font=default_font, bg="#f7e9f3")
+label_imie_szczegoly_obiektu.grid(row=1, column=0, sticky="ew")
 
-label_imie_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
-label_imie_szczegoly_obiektu_wartosc.grid(row=1, column=1, sticky=W)
+label_imie_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...", font=default_font, bg="#f7e9f3")
+label_imie_szczegoly_obiektu_wartosc.grid(row=1, column=1, sticky="ew")
 
-label_lokalizacja_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Lokalizacja: ")
-label_lokalizacja_szczegoly_obiektu.grid(row=1, column=2, sticky=W)
+label_lokalizacja_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Lokalizacja: ", font=default_font, bg="#f7e9f3")
+label_lokalizacja_szczegoly_obiektu.grid(row=1, column=2, sticky="ew")
 
-label_lokalizacja_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
-label_lokalizacja_szczegoly_obiektu_wartosc.grid(row=1, column=3, sticky=W)
+label_lokalizacja_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...", font=default_font, bg="#f7e9f3")
+label_lokalizacja_szczegoly_obiektu_wartosc.grid(row=1, column=3, sticky="ew")
 
-label_posty_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text= "Posty: ")
-label_posty_szczegoly_obiektu.grid(row=1, column=4, sticky=W)
+label_posty_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text= "Posty: ", font=default_font, bg="#f7e9f3")
+label_posty_szczegoly_obiektu.grid(row=1, column=4, sticky="ew")
 
-label_posty_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
-label_posty_szczegoly_obiektu_wartosc.grid(row=1, column=5, sticky=W)
+label_posty_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...", font=default_font, bg="#f7e9f3")
+label_posty_szczegoly_obiektu_wartosc.grid(row=1, column=5, sticky="ew")
 
 for i in range(6):
     ramka_szczegoly_obiektu.columnconfigure(i, weight=1)
